15a2d19da5decaf62610bea1a0b728067761d28e
karasikrus <karasikrus@gmail.com>
2019-04-22 23:44:27 +0300

    сомнительные стили

--- src/app/mock-gameRooms.ts
+++ src/app/mock-gameRooms.ts
@@ -27,5 +27,18 @@ export const ROOMS: GameRoom[] = [
     playerTurn: false,
     hostTurn: true,
     activePlayer: null
+  },
+  { id: 8,
+    currentRound: null,
+    gameRounds: null,
+    host: {name: 'Kolya', avatarPictureUrl: null},
+    maxPlayers: 3,
+    name: 'Веселая игра(нет)',
+    password: null,
+    players: [{user: {name: 'a', avatarPictureUrl: null}, score: 1100}, {user: {name: 'b', avatarPictureUrl: null}, score: 0}, {user: {name: 'x', avatarPictureUrl: null}, score: -300}],
+    state: GameRoomState.WaitingForStart,
+    playerTurn: false,
+    hostTurn: true,
+    activePlayer: null
   }
 ];
