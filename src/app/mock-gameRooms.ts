9d4a4e61009674c0deacda336dff6290fe5e2c30
karasikrus <karasikrus@gmail.com>
2019-04-24 12:34:07 +0300

    small changes in data structure

--- src/app/mock-gameRooms.ts
+++ src/app/mock-gameRooms.ts
@@ -6,7 +6,39 @@ export const ROOMS: GameRoom[] = [
     currentRound: 0,
     currentCategoryId: null,
     currentQuestionId: null,
-    gameRounds: null,
+    gameRounds: [{name: 'разминочка', questionCategories: [{
+      name: 'Имена', id: 0, comment: '', questions: [
+          {
+            id: 0, comment: '', answered: false, answeringPlayer: null, choosingPlayer: null, text: 'Как зовут автора пака?',
+            answerText: 'Иван', cost: 100, type: null
+          },
+          {
+            id: 1, comment: '', answered: false, answeringPlayer: null, choosingPlayer: null,
+            text: 'Как звали последнего царя династии Рюриковичей?',
+            answerText: 'Иван', cost: 300, type: null
+          },
+          {
+            id: 2, comment: '', answered: false, answeringPlayer: null, choosingPlayer: null, text: 'Самое популярное мужское имя в России',
+            answerText: 'Александр', cost: 500, type: null
+          },
+        ]
+      },
+        {
+          name: 'Фамилии', id: 1, comment: '', questions: [
+            {
+              id: 0, comment: '', answered: false, answeringPlayer: null, choosingPlayer: null, text: 'Династия российских императоров',
+              answerText: 'Романовы', cost: 100, type: null
+            },
+            {
+              id: 1, comment: '', answered: false, answeringPlayer: null, choosingPlayer: null, text: 'Фамилия Ленина',
+              answerText: 'Ульянов', cost: 300, type: null
+            },
+            {
+              id: 2, comment: '', answered: false, answeringPlayer: null, choosingPlayer: null, text: 'Самое популярная фамилия в России',
+              answerText: 'Смирнов', cost: 500, type: null
+            },
+          ]
+        }]}],
     host: {name: 'vanya', avatarPictureUrl: null},
     maxPlayers: 3,
     name: 'room one',
